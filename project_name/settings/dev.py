5377adaa5fe978992b6f07f068681ae559a3c329
Morgan <morgan.bohn@unistra.fr>
2014-11-20 16:27:11 +0100

    configure debug toolbar to work with vagrant (dev env.)

--- project_name/settings/dev.py
+++ project_name/settings/dev.py
@@ -56,3 +56,13 @@ TEST_RUNNER = 'django_coverage.coverage_runner.CoverageRunner'
 
 DIPSTRAP_VERSION = environ.get('DIPSTRAP_VERSION', 'latest')
 DIPSTRAP_STATIC_URL += '%s/' % DIPSTRAP_VERSION
+
+#################
+# Debug toolbar #
+#################
+
+DEBUG_TOOLBAR_PATCH_SETTINGS = True
+MIDDLEWARE_CLASSES += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+INTERNAL_IPS = ('127.0.0.1', '0.0.0.0')
--- project_name/urls.py
+++ project_name/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
@@ -10,3 +10,10 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+# debug toolbar for dev
+if settings.DEBUG and 'debug_toolbar'in settings.INSTALLED_APPS:
+    import debug_toolbar
+    urlpatterns += patterns('',
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    )
