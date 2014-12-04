4191966c8b80fcb03dc7fde295b059ca7e1485b0
Thomas Baguet <baguet@unistra.fr>
2014-12-04 14:47:24 +0100

    remove verbose output in pydiploy

--- fabfile.py
+++ fabfile.py
@@ -30,6 +30,7 @@ env.timezone = 'Europe/Paris'  # timezone for remote
 env.keep_releases = 2  # number of old releases to keep before cleaning
 env.extra_goals = ['preprod']  # add extra goal(s) to defaults (test,dev,prod)
 env.dipstrap_version = 'latest'
+env.verbose_output = False # True for verbose output
 
 # optional parameters
 
