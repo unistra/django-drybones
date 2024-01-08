dc7fc30c08468bb54b9b70506efc754863a91e6f
dbessey <daniel.bessey@unistra.fr>
2024-01-08 14:23:55 +0100

    Fix test runner in run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -2,19 +2,20 @@ import os
 import sys
 
 import django
+from django.conf import settings
+from django.test.utils import get_runner
 
 os.environ['DJANGO_SETTINGS_MODULE'] = '{{ project_name }}.settings.unittest'
 django.setup()
 
-from django.conf import settings
 
 
 """
 Run tests script
 """
 
-test_runner = settings.TEST_RUNNER(pattern='test_*.py', verbosity=2,
-                                   interactive=True, failfast=False)
+TestRunner = get_runner(settings)
+test_runner = TestRunner(pattern='test_*.py', verbosity=2, interactive=True, failfast=False)
 
 test_apps = list(settings.LOCAL_APPS)
 test_apps = test_apps if len(sys.argv) <= 1 else sys.argv[1:]
